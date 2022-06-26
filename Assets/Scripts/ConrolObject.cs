5382d57d8ad135ee4033c224b2d8b2744a51788e
SSD\ekhuu <ekhuu@strongtie.com>
2022-06-26 12:09:58 +0700

    Chinh sua cau truc voi them script

--- Assets/Scripts/ConrolObject.cs
+++ Assets/Scripts/ConrolObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewScript : MonoBehaviour
+public class ConrolObject : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
