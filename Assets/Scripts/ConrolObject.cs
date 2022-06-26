05361bbc0ec0e941da63c1c5a5a58a73afc796ed
SSD\ekhuu <ekhuu@strongtie.com>
2022-06-26 12:15:36 +0700

    Click random color object

--- Assets/Scripts/ConrolObject.cs
+++ Assets/Scripts/ConrolObject.cs
@@ -7,12 +7,13 @@ public class ConrolObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //demo 02
+        if (Input.GetMouseButtonUp(0)) transform.GetComponent<MeshRenderer>().material.color = Random.ColorHSV(0, 1);
     }
 }
