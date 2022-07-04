1dc4514a41a514456fec515372019a53c82352f5
SSD\ekhuu <ekhuu@strongtie.com>
2022-07-04 11:12:07 +0700

    Update code NewScrtps

--- Assets/Scripts/NewScritps.cs
+++ Assets/Scripts/NewScritps.cs
@@ -7,12 +7,17 @@ public class NewScritps : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //update every frame
+    }
+
+    public void NewFeature()
+    {
+
     }
 }
